eea22871a7d43d6dff307118be278c7731b90a23
Karan09823 <karankumar514418@gmail.com>
2025-04-06 16:57:23 +0530

    HMD backend

---
+++ backend/routes/login.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const User = require("../models/User");
+const Doctor = require("../models/Doctor");
+const Admin = require("../models/Admin"); // Add this line
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const jwt_key = process.env.jwt_key;
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { email, password, role } = req.body;
+
+  console.log("Received data:", req.body);
+
+  try {
+    let user;
+    if (role === "doctor") {
+      user = await Doctor.findOne({ email });
+    } else if (role === "admin") {
+      user = await Admin.findOne({ email });
+    } else {
+      user = await User.findOne({ email, role });
+    }
+
+    if (!user) {
+      return res.status(400).send({ error: "Invalid email or role" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).send({ error: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      jwt_key,
+      { expiresIn: "24h" }
+    );
+    res.send({ token, role: user.role });
+  } catch (error) {
+    res.status(500).send({ error: "Server error" });
+  }
+});
+
+module.exports = router;
